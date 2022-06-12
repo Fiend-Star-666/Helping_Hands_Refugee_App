db0243ef572c25bad1ae10e02cde3f7f015be04f
Fiend-Star-666 <gsingh3_be18@thapar.edu>
2022-06-12 19:28:13 +0530

    apis

--- src/services/RefugeeServices.js
+++ src/services/RefugeeServices.js
@@ -6,6 +6,15 @@ class RefugeeServices {
     createTask(task) {
         return axios.post(API_BASE_URL + '/refugee/create/task', task);
     }
+
+    viewAllTasks(){
+        return axios.get(API_BASE_URL + '/refugee/task/viewall');
+    }
+
+    viewTasksViaAccountId(accountId){
+        return axios.get(API_BASE_URL + '/refugee/task/view/' + accountId);
+    }
+
 }
 
 export default new RefugeeServices();
--- src/services/Volunteer.service.js
+++ src/services/Volunteer.service.js
@@ -6,5 +6,14 @@ class VolunteerServices {
     createService(service) {
         return axios.post(API_BASE_URL + '/volunteer/create/service', service);
     }
+
+    viewAllServices(){
+        return axios.get(API_BASE_URL + '/volunteer/service/viewall');
+    }
+
+    viewServicesViaAccountId(accountId){
+        return axios.get(API_BASE_URL + '/volunteer/service/view/' + accountId);
+    }
+
 }
 export default new VolunteerServices();
