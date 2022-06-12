3ab65ea6bbc5aeb5db9b01aac64e6461fb3c7375
Fiend-Star-666 <gsingh3_be18@thapar.edu>
2022-06-12 05:57:41 +0530

    create volunteer done

--- src/App.js
+++ src/App.js
@@ -16,6 +16,20 @@ import EventBus from './common/EventBus';
 import error from './components/error';
 import Home from "./components/securityComponents/home.component.js";
 
+import Logo from "./icons/logo.svg";
+import Contact from "./icons/contact_illustration.svg";
+import HandsImg from "./icons/hands.svg";
+import HouseDrawn from "./icons/house_hand_drawn.svg";
+import House from "./icons/house_illustration.svg";
+import Join from "./icons/join_illustration.svg";
+import PurpleBackdrop from "./icons/lightpurple.svg";
+import MapConnect from "./icons/map_connecting_world_illustration.svg";
+import Motherhood from "./icons/motherhood_illustration.svg";
+
+
+
+
+
 //TEMPORARY
 import CreateRefugee from './components/tempCreateRefugee';
 import CreateVolunteer from './components/tempCreateVolunteer';
@@ -30,7 +44,6 @@ class App extends Component{
       showVolunteerBoard: false,
       showAdminBoard: false,
       currentUser: undefined
-
     };
   }
 
@@ -89,93 +102,93 @@ class App extends Component{
       <div>
         <Router>
 
-        <nav className="navbar navbar-expand navbar-light bg-light">
-          <Link to={"/home"} className="navbar-brand">
-            Helping Hands
-          </Link>
-          <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to={"/home"} className="nav-link">
-                Home
-              </Link>
-            </li>
-
-            {showAdminBoard && (
-              <li className="nav-item">
-                <Link to={"/admin"} className="nav-link">
-                  Admin Board
-                </Link>
-              </li>
-            )}
-
-            {showVolunteerBoard && (
-              <li className="nav-item">
-                <Link to={"/volunteer"} className="nav-link">
-                  Volunteer Board
-                </Link>
-              </li>
-            )}
-            
-            {showRefugeeBoard && (
-              <li className="nav-item">
-                <Link to={"/refugee"} className="nav-link">
-                  Refugee Board
-                </Link>
-              </li>
-            )}
-{console.log('showAdminBoard',showAdminBoard)}
-{console.log('showRefugeeBoard',showRefugeeBoard)}
-{console.log('showVolunteerBoard',showVolunteerBoard)}
-          </div>
-
-          {currentUser ? (
-            <div className="navbar-nav ml-auto">
-
-            {showAdminBoard && (
-            <li className="nav-item">
-            <Link to={"/account/register/admin"} className="nav-link">
-              Register Admin
+          <nav className="navbar navbar-expand navbar-light bg-light">
+            <Link to={"/home"} className="navbar-brand">
+              <img src={Logo} alt="Helping Hand" style={{width: "100px"}} />
             </Link>
-            </li>
+            <div className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link to={"/home"} className="nav-link">
+                  Home
+                </Link>
+              </li>
+
+              {showAdminBoard && (
+                <li className="nav-item">
+                  <Link to={"/admin"} className="nav-link">
+                    Admin Board
+                  </Link>
+                </li>
+              )}
+
+              {showVolunteerBoard && (
+                <li className="nav-item">
+                  <Link to={"/volunteer"} className="nav-link">
+                    Volunteer Board
+                  </Link>
+                </li>
+              )}
+              
+              {showRefugeeBoard && (
+                <li className="nav-item">
+                  <Link to={"/refugee"} className="nav-link">
+                    Refugee Board
+                  </Link>
+                </li>
+              )}
+  {console.log('showAdminBoard',showAdminBoard)}
+  {console.log('showRefugeeBoard',showRefugeeBoard)}
+  {console.log('showVolunteerBoard',showVolunteerBoard)}
+            </div>
+
+            {currentUser ? (
+              <div className="navbar-nav ml-auto">
+
+              {showAdminBoard && (
+              <li className="nav-item">
+              <Link to={"/account/register/admin"} className="nav-link">
+                Register Admin
+              </Link>
+              </li>
+              )}
+            
+
+                <li className="nav-item">
+                  <Link to={"/profile"} className="nav-link">
+                    Profile
+                  </Link>
+                </li>
+  
+                <li className="nav-item">
+                  <a href="/home" className="nav-link" onClick={this.logOut}>
+                    LogOut
+                  </a>
+                </li>
+              </div>
+            ) : (
+              <div className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link to={"/login"} className="nav-link">
+                    Login
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link to={"/account/register/refugee"} className="nav-link">
+                    Sign Up Refugee
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link to={"/account/register/volunteer"} className="nav-link">
+                    Sign Up Volunteer
+                  </Link>
+                </li>
+
+              </div>
             )}
-          
 
-              <li className="nav-item">
-                <Link to={"/profile"} className="nav-link">
-                  Profile
-                </Link>
-              </li>
- 
-              <li className="nav-item">
-                <a href="/home" className="nav-link" onClick={this.logOut}>
-                  LogOut
-                </a>
-              </li>
-            </div>
-          ) : (
-            <div className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link to={"/login"} className="nav-link">
-                  Login
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link to={"/account/register/refugee"} className="nav-link">
-                  Sign Up Refugee
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link to={"/account/register/volunteer"} className="nav-link">
-                  Sign Up Volunteer
-                </Link>
-              </li>
-
-            </div>
-          )}
-
-        </nav>
+          </nav>
 
             <div className="container">   
                 <Switch>
--- src/components/tempCreateVolunteer.jsx
+++ src/components/tempCreateVolunteer.jsx
@@ -1,7 +1,94 @@
 import React from "react";
 import AccountService from "../services/AccountService";
 
-class TempCreateRefugee extends React.Component {
-    
+class TempCreateVolunteer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            emailId: '',
+            password: '',
+            name: '',
+            phoneNumber: '',
+            streetAddress: '',
+            city: '',
+            state: '',
+            zipCode: '',
+            country: '',
+
+        }
+        this.changeHandler = this.changeHandler.bind(this);
+    }
+
+    saveVolunteer = (e) => {
+        e.preventDefault();
+
+        let Volunteer= { emailId: this.state.emailId, password: this.state.password, name: this.state.name, phoneNumber: this.state.phoneNumber,
+                         streetAddress: this.state.streetAddress, city: this.state.city, state: this.state.state, zipCode: this.state.zipCode,
+                         country: this.state.country};
+
+        console.log("Volunteer" + JSON.stringify(Volunteer));
+
+        AccountService.createVolunteer(Volunteer).then(response => {
+            this.props.history.push('/login');
+        }).catch(function (error) {
+            alert('Please Check your Details or An account with Same details already exists');
+        });
+    }
+
+    changeHandler = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+
+    render(){
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h2>Create a New Volunteer</h2>
+                        <form onSubmit={this.saveVolunteer}>
+                            <div className="form-group">
+                                <label>Email Id</label>
+                                <input type="text" className="form-control" name="emailId" value={this.state.emailId} onChange={this.changeHandler} />
+                            </div>
+                            <div className="form-group">
+                                <label>Password</label>
+                                <input type="password" className="form-control" name="password" value={this.state.password} onChange={this.changeHandler} />
+                            </div>
+                            <div className="form-group">
+                                <label>Name</label>
+                                <input type="text" className="form-control" name="name" value={this.state.name} onChange={this.changeHandler} />
+                            </div>
+                            <div className="form-group">
+                                <label>Phone Number</label>
+                                <input type="text" className="form-control" name="phoneNumber" value={this.state.phoneNumber} onChange={this.changeHandler} />
+                            </div>
+                            <div className="form-group">
+                                <label>Street Address</label>
+                                <input type="text" className="form-control" name="streetAddress" value={this.state.streetAddress} onChange={this.changeHandler} />
+                            </div>
+                            <div className="form-group">
+                                <label>City</label>
+                                <input type="text" className="form-control" name="city" value={this.state.city} onChange={this.changeHandler} />
+                            </div>
+                            <div className="form-group">
+                                <label>State</label>
+                                <input type="text" className="form-control" name="state" value={this.state.state} onChange={this.changeHandler} />
+                            </div>
+                            <div className="form-group">
+                                <label>Zip Code</label>
+                                <input type="text" className="form-control" name="zipCode" value={this.state.zipCode} onChange={this.changeHandler} />
+                            </div>
+                            <div className="form-group">
+                                <label>Country</label>
+                                <input type="text" className="form-control" name="country" value={this.state.country} onChange={this.changeHandler} />
+                            </div>
+                            <button type="submit" className="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
-export default TempCreateRefugee;
+export default TempCreateVolunteer;
