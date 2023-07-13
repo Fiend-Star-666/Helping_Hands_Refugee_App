df92a804347c39096e2f0f6e67964e678831728c
Gursimran Singh <69344628+Fiend-Star-666@users.noreply.github.com>
2023-07-13 05:38:05 +0530

    Update start-backend.sh

--- start-backend.sh
+++ start-backend.sh
@@ -12,5 +12,11 @@ wait_for_mysql() {
 # Check MySQL availability
 wait_for_mysql
 
+# Import the SQL file
+echo "Running SQL script..."
+mysql -h localhost -P 3306 -u root -p1234 -e "source /app/create_databases.sql"
+echo "SQL script executed successfully!"
+
+
 # Start the Spring Boot application
 java -Djava.security.egd=file:/dev/./urandom -jar ./Back-end/athena/target/athena-0.0.1-SNAPSHOT.jar
